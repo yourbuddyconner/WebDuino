3a7d11e14fbdaaed9b57fe2de33c17d7246886b5
Conner Swann <me@connerswann.me>
2014-05-24 23:14:16 -0700

    finally got the api access point working

--- WebDuino.js
+++ WebDuino.js
@@ -20,16 +20,26 @@ if (Meteor.isClient) {
 if (Meteor.isServer) { 
     Router.map(function(){
       this.route('accessPoint',{
-        path: '/api/:_id',
+        path: '/api/:_id', // expects a project ID
+        where: 'server',
         action: function(){
-          var project = Projects.findOne({_id: this.params._id}).fetch();
+          var project = Projects.findOne({_id: this.params._id}); //find the corresponding project
           if(project){
-            Sensors = Sensors.find({}, {parentId: project._id}).fetch(); // An array of Sensors that belong to the project
-            Readings.insert({
-
-            })
+            var sensorArray = Sensors.find({}, {parentId: this.params._id}).fetch(); // An array of Sensors that belong to the project
+            var time = new Date();
+            var params = this.params;
+            for (var i =0 ; i < sensorArray.length; i++) {
+              var theID = sensorArray[i]._id
+              var newReading = {
+                parentId: sensorArray[i]._id,
+                timestamp: time,
+                data: [this.params[theID]]
+              };
+              Readings.insert(newReading); 
+            };//end for
           }
+          return "Thank you!"
         }
-      });
+      });//end route
     });
 }
