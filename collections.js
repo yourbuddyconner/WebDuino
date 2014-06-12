a672f56d898ad242c647e9f9e78b79f0e4267333
Conner Swann <me@connerswann.me>
2014-06-11 17:03:02 -0700

    changed how collections are stored. Projects, Sensors and Readings are now three discrete collections

--- collections.js
+++ collections.js
@@ -1,4 +1,5 @@
 Projects = new Meteor.Collection("projects");
+Sensors = new Meteor.Collection("sensors");
 Readings = new Meteor.Collection("readings");
 
 // THE LINE BELOW SHOULD BE COMMENTED OUT!
@@ -9,31 +10,70 @@ Schemas = {};
 Schemas.project = new SimpleSchema({
   name: {
     type: String,
-    label: "Name"
+    label: "Name",
+    autoValue: function(){
+      if(this.isUpdate){
+        console.log(this.value);
+      }
+    }
   },
-  owner: { //<-- This gets inserted manually in Meteor.methods
+  owner: {
     type: String,
     label: "Owner",
     autoValue: function() {
       if (this.value){
-        console.log("The owner is set to " + this.value)
+        //console.log("The owner is set to " + this.value)
         return this.value
       }
       else{
         return this.userId
       }
     }
-  },
-  sensors: {
-    type: [Object],
-    minCount: 0,
-    optional: true
-  },
-  "sensors.$.name": {
-    type: String,
-  },
-  "sensors.$.type": {
+  }
+// This was the first way I tried to handle sensors in the DB,
+// not ready to count it out yet though. 
+//
+// sensors: {
+//   type: [Object],
+//   minCount: 0,
+//   autoValue: function(){
+//     if(this.isInsert && !this.value){
+//       return [];
+//     }
+//     if(this.isUpdate){
+//       console.log(this);
+//     }
+//   }
+// },
+// "sensors.$.name": {
+//   type: String
+// },
+// "sensors.$.type": {
+//   type: String
+// }
+});
+Schemas.sensor = new SimpleSchema({
+  parentProject: {
     type: String
+  },
+  name: {
+    type: String
+  },
+  type: {
+    type: String
+  },
+  owner: {
+    type: String,
+    label: "Owner",
+    autoValue: function() {
+      if (this.value){
+        //console.log("The owner is set to " + this.value)
+        return this.value
+      }
+      else{
+        return this.userId
+      }
+    }
   }
 });
 Schemas.reading = new SimpleSchema({
@@ -44,14 +84,14 @@ Schemas.reading = new SimpleSchema({
   owner: {
     type: String
   },
-  parentProject: {
+  parentSensor: {
     type: String,
-    label: "ID of the project this reading belongs to"
+    label: "ID of the sensor this reading belongs to"
   },
-  sensorName: {
-    type: String,
-    label: "Name of the parent sensor in project.sensors"
-  },
+// sensorName: {
+//   type: String,
+//   label: "Name of the parent sensor in project.sensors"
+// },
   timestamp: {
     type: Date,
     label: "when this reading was saved to the database"
@@ -63,4 +103,5 @@ Schemas.reading = new SimpleSchema({
 });
 
 Projects.attachSchema(Schemas.project);
+Sensors.attachSchema(Schemas.sensor);
 Readings.attachSchema(Schemas.reading);
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,4 +1,11 @@
+/* 
+	Seed the DB with an administrative user and some projects/sensors/readings
+	to play and debug with. This file is meant to run completely at the first 
+	run, if there's errors, reset the DB ("meteor reset") and restart Meteor. 
+*/
+
 Meteor.startup( function(){
+	// If there are no users
 	if(Meteor.users.find().count() === 0){
 		Accounts.createUser({
 			username: "admin",
@@ -12,42 +19,62 @@ Meteor.startup( function(){
 		});
 		console.log("Default user created succussfully!")
 	}
+	// If there are no projects 
 	if(Projects.find().count() === 0){
-
+		// Find the admin user we just made
 		var admin = Meteor.users.findOne({username: "admin"});
-		var newSensor = {
-			name: "My Potentiometer",
-			type: "Potentiometer" 
-		}
 		var newProject = {
 			name: "My First Project",
 			owner: admin._id,
-			sensors: [
-				{
-					name: "My Potentiometer",
-					type: "Potentiometer" 
-				},
-				{
-					name: "My Accelerometer",
-					type: "Accelerometer"
-				}
-			]
+			// sensors: [
+			// 	{
+			// 		name: "My Potentiometer",
+			// 		type: "Potentiometer" 
+			// 	},
+			// 	{
+			// 		name: "My Accelerometer",
+			// 		type: "Accelerometer"
+			// 	}
+			// ]
 		}
-		console.log(newProject);
+		console.log("Here's the project: ", newProject);
 		var projectId = Projects.insert(newProject);
 	}
+	if (Sensors.find().count() === 0){
+		parent = Projects.findOne({_id: projectId});
+		var newSensor = {
+			parentProject: parent._id,
+			name: "My Potentiometer",
+			type: "Potentiometer",
+			owner: admin._id
+		}
+		var newSensor1 = {
+			parentProject: parent._id,
+			name: "Humidity Sensor",
+			type: "Humidity",
+			owner: admin._id 
+		}
+		var sensorOne = Sensors.insert(newSensor);
+		var sensorTwo = Sensors.insert(newSensor1);
+		console.log("Added the Sensors!")
+}
 	if(Readings.find().count() === 0){
+		sensorOne = Sensors.findOne({_id: sensorOne});
+		sensorTwo = Sensors.findOne({_id: sensorTwo});
+		console.log("Sensor One: ", sensorOne);
+		console.log("Sensor Two: ", sensorTwo);
+
 		var newReading1 = {
 			owner: admin._id,
-			parentProject: projectId,
-			sensorName: newProject.sensors[0].name,
+			parentSensor: sensorOne._id,
+			//sensorName: newProject.sensors[0].name,
 			timestamp: 0,
 			data: 0
 		};
 		var newReading2 = {
 			owner: admin._id,
-			parentProject: projectId,
-			sensorName: newProject.sensors[1].name,
+			parentSensor: sensorTwo._id,
+			//sensorName: newProject.sensors[1].name,
 			timestamp: 0,
 			data: 1
 		};
@@ -55,13 +82,16 @@ Meteor.startup( function(){
   			return Math.floor(Math.random() * (max - min + 1)) + min;
 		}
 		for(var i=0; i<30; i++){
-			newReading1.data = getRandomInt(1, 100);
-			newReading2.data = newReading2.data+2;
-			newReading1.timestamp = new Date();
-			newReading2.timestamp = new Date();
-			Readings.insert(newReading1);
-			Readings.insert(newReading2);
+			Meteor.setTimeout(function(){
+				newReading1.data = getRandomInt(1, 100);
+				newReading2.data = newReading2.data*2;
+				newReading1.timestamp = new Date();
+				newReading2.timestamp = new Date();
+				Readings.insert(newReading1);
+				Readings.insert(newReading2);
+			}, 1000*i)
 		}
-	console.log("Database Seeded! Do your worst.")
+	console.log("Readings Seeded.")
+	console.log("All Done! Do your worst.")
 	}
 });
